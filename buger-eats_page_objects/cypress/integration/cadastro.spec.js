24129aec262e64dd43e58e54cba8518fbaee2647
Patricia <patysonar@gmail.com>
2022-03-07 19:32:33 -0300

    Trabalhando com Fixtures

--- buger-eats_page_objects/cypress/integration/cadastro.spec.js
+++ buger-eats_page_objects/cypress/integration/cadastro.spec.js
@@ -1,77 +1,36 @@
- ////**** Aula - Export default new Page ****////
+////**** Aula - Trabalhando com Fixtures ****////
 import signup from '../pages/SignupPage' //Instancia  da página para os casos de testes
 
 describe('Cadastro', () => {      /// Se chama hero function
 
-    it('Usuario deve se tornar um entregador', () => {
-        ////**** Aula - Refatorando a massa de testes para inglês ****////
+    beforeEach(function () {
+        cy.fixture('deliver').then((d) => {  //pega o resultado da massa de teste. (d) argumento
+            this.deliver = d  //this ter acesso a informação
+        })
+    })
+
+    it('Usuario deve se tornar um entregador', function () {
         var deliver = {
-            name: 'Patricia Souza',
-            cpf: '00000014141',
-            email: 'patysonar@gmail.com',
-            whatsapp: '11999999999',
-
-            ////****Aula  - Preenchendo endereco ****////
-            address: {
-                postalcode: '40450330',
-                street: 'Travessa Polivalente',
-                number: '1B',
-                details: 'Casa Térreo',
-                district: 'Uruguai',
-                city_state: 'Salvador/BA'
-
-            },
-            ////**** Aula - Combinando CSS Selector com Texto ****////
-            delivery_method: 'Moto',  // Modo de entrega - informar modo de entrega desejada - Moto, Bicicleta e Van/Carro
-
-            ////****Aula - Upload de arquivos ****////
-            cnh: 'cnh-digital.jpg'
 
         }
 
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato'    //// Valor constante que não vai mudar
         signup.modalContentShouldBe(expectedMessage)
     })
 
-
-    it('CPF Incorreto', () => {
-
-        ////**** Aula - Refatorando a massa de testes para inglês ****////
+    it('CPF Incorreto', function () {
         var deliver = {
-            name: 'Patricia Souza',
-            cpf: '000000141AA',
-            email: 'patysonar@gmail.com',
-            whatsapp: '11999999999',
-
-            ////****Aula  - Preenchendo endereco ****////
-
-            address: {
-                postalcode: '40450330',
-                street: 'Travessa Polivalente',
-                number: '1B',
-                details: 'Casa Térreo',
-                district: 'Uruguai',
-                city_state: 'Salvador/BA'
-
-            },
-            ////**** Aula - Combinando CSS Selector com Texto ****////
-
-            delivery_method: 'Moto',  // Modo de entrega - informar modo de entrega desejada - Moto, Bicicleta e Van/Carro
-
-            ////****Aula - Upload de arquivos ****////
-            cnh: 'cnh-digital.jpg'
 
         }
 
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.cpf_inv)
         signup.submit()
         signup.alertMessageShoudBe('Oops! CPF inválido')
-
 
     })
 })
