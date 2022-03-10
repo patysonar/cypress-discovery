77dbf6069f6056145c0ed4aab86f93c02f7e65ed
Patricia <patysonar@gmail.com>
2022-03-10 17:44:11 -0300

    Page Object é bão demais

--- buger-eats-qa_page_objects/cypress/pages/SignupPage.js
+++ buger-eats-qa_page_objects/cypress/pages/SignupPage.js
@@ -12,7 +12,7 @@ class SignupPage {  // Vai representar a página de cadastro
     fillForm(deliver) {  // Vai preencher todo formulario de cadastro
 
         ////**** Aula - Preenchendo campos ****////
-        cy.get('input[name = "name"]').type(deliver.name)
+        cy.get('input[name = "fullName"]').type(deliver.name)
         cy.get('input[name = "cpf"]').type(deliver.cpf)
         cy.get('input[name = "email"]').type(deliver.email)
         cy.get('input[name = "whatsapp"]').type(deliver.whatsapp)
